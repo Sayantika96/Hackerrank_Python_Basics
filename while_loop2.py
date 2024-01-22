4da5636df7499b2cf322f31d1dac474e9cf6457f
Sayantika Saha <50393693+Sayantika96@users.noreply.github.com>
2024-01-21 20:39:07 -0800

    Update while_loop2.py

--- while_loop2.py
+++ while_loop2.py
@@ -1,3 +1,5 @@
+Problem : Write a program to accept a number from a user and calculate the sum of all numbers from 1 to a given number
+
 user_input = int(input("Enter a valid number: "))
 s = 0
 for i in range(1, user_input + 1, 1):
