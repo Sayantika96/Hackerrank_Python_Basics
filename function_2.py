974df1161d30aaf1a49a7ea788b11a0eec3a8a19
Sayantika Saha <50393693+Sayantika96@users.noreply.github.com>
2024-02-01 20:17:04 -0800

    function_2.py

--- function_2.py
+++ function_2.py
@@ -1,3 +1,7 @@
+Problem: Implement a simple loan eligibility function that takes three parameters - age, income, and credit_score. If the applicant's age is greater than or equal to 19 years, their annual income is more than $100,00, and their credit_score is greater than or equal to 650, then they are eligible for the loan otherwise not. 
+Apply this function to a dictionary having keys 'age', 'income', and 'credit_score'.
+
+Solution:
 def check_loan_eligible(age, income, credit_score):
     if age >= 19 and income >= 10000 and credit_score >= 650:
         return True
